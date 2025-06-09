fb4cf20087b7dda8405ebbc4108db96b31b8c1f4
Tdansistod <lautaronuriel@gmail.com>
2025-06-09 15:33:49 -0300

    logo from server

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,8 @@
-"use client";
-import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
 import styles from "@/styles/header.module.css";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
+const logoUrl =
+  "https://pbmvrjvjhablmelovyoc.supabase.co/storage/v1/object/public/assets//logo%20transparent.webp";
 export default function Header() {
-  const [logoUrl, setLogoUrl] = useState(null);
-
-  useEffect(() => {
-    const getLogo = async () => {
-      const { data } = await supabase.storage
-        .from("assets")
-        .getPublicUrl("logo transparent.webp");
-
-      setLogoUrl(data.publicUrl);
-    };
-
-    getLogo();
-  }, []);
-
   return (
     <header className={styles.header}>
       {logoUrl && <img src={logoUrl} alt="Logo" className={styles.logo} />}
