20aa6aa6164803b35981c0a1e2ffaabb02bf1a4a
Tdansistod <lautaronuriel@gmail.com>
2025-06-09 16:38:13 -0300

    share config added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,41 @@ import "./globals.css";
 import Header from "@/components/header.js";
 
 export const metadata = {
-  title: "Tienda Online de Electricidad e Iluminación | Eletrodel",
+  title: "Tienda Online de Electricidad e Iluminación | Electrodel",
   description:
-    "Descubrí productos de electricidad e iluminación para tu hogar o negocio. En Eletrodel encontrás calidad, buenos precios y atención personalizada.",
+    "Descubrí productos de electricidad e iluminación para tu hogar o negocio. En Electrodel encontrás calidad, buenos precios y atención personalizada.",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Tienda Online de Electricidad e Iluminación | Electrodel",
+    description:
+      "Descubrí productos de electricidad e iluminación para tu hogar o negocio. En Electrodel encontrás calidad, buenos precios y atención personalizada.",
+    url: "https://eletrodel.com.ar",
+    siteName: "Eletrodel",
+    images: [
+      {
+        url: "https://eletrodel.com.ar/logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Logo de Eletrodel",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tienda Online de Electricidad e Iluminación | Electrodel",
+    description:
+      "Descubrí productos de electricidad e iluminación para tu hogar o negocio. En Electrodel encontrás calidad, buenos precios y atención personalizada.",
+    images: ["https://eletrodel.com.ar/logo.jpg"],
+  },
 };
+
 const inter = Inter({
-  subsets: ["latin"], // Podés ajustar esto según el idioma que uses
+  subsets: ["latin"],
   display: "swap",
 });
 export default function RootLayout({ children }) {
