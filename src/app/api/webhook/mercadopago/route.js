398ca046ded45907e303e75235a2b8b1cc68f31c
Tdansistod <lautaronuriel@gmail.com>
2025-06-28 06:26:27 -0300

    api adjust

--- src/app/api/webhook/mercadopago/route.js
+++ src/app/api/webhook/mercadopago/route.js
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabaseServer";
+import { createClient } from "@supabase/supabase-js";
 
 export async function POST(req) {
-  const supabase = await createClient();
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+  );
   try {
     const body = await req.json();
     const paymentId = body?.data?.id;
