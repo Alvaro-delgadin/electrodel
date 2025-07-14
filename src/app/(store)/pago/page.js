1ff617576f3360cd52fa51c1ec1f1a3e82266b19
Alvaro-delgadin <alvaro_delgadin@hotmail.com>
2025-07-14 20:26:58 -0300

    mp logo size fix

--- src/app/(store)/pago/page.js
+++ src/app/(store)/pago/page.js
@@ -216,7 +216,7 @@ export default function CheckoutPage() {
                 <CircularProgress size={20} color="inherit" />
               ) : (
                 <Image
-                  style={{ height: "100%", width: "auto" }}
+                  style={{ height: "3rem", width: "auto" }}
                   width={100}
                   height={50}
                   alt="Mercado Pago Logo"
